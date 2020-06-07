3956959300872feceec54d7d30e7d899256c5691
Gustavo Dias <gustavodias2@gmail.com>
2020-06-07 04:44:57 -0300

    Inserindo arquivos da Aula03

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,69 +1,15 @@
 import express from 'express';
-import routes from './routes';
+import path from 'path'
+import cors from 'cors';
+
+import routes from './routes'
 
 const app = express();
 
-app.use(express.json());
-app.use(routes);
+app.use(cors());
+app.use(express.json())
+app.use(routes)
 
-app.listen(3333);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
-/**
- 
-// Rota: endereço completo da requisição
-// Recurso: qual entidade estamos acessando do sistema
-
-// GET: buscar uma ou mais informações do backend
-// POST: criar uma nova informação no backend
-// PUT: atualizar uma informação existente no backend
-// DELETE: remover uma informação do backend
-
-// POST http://localhost:3333/users = criar um usuário
-// GET http:localhost:3333/users = listar usuários
-// GET http:localhost:3333/users/5 = buscar dados do usuário com ID = 5
-
-// Request Param: Parâmetros que vem na própria rota normalmente OBRIGATÓRIO que identificam um recurso 
-// Query Param: Parâmetros que vem na própria rota geralmente OPCIONAIS para filtros, paginação
-// Request Body: Parâmetros para criação / atualização de informações
-
-// Banco - Select * FROM users WHERE name = 'Marcia'
-// knex('users').WHERE('name','Marcia').select('*')
-
-const users = [
-    'Diego',    // 0
-    'Daniel',   // 1
-    'Claudia',  // 2
-    'Marcia'    // 3
-];
-
-app.get('/users',(request, response) =>{
-    const search = String(request.query.search);
-
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-
-    // JSON
-    return response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) =>{
-    const id = Number(request.params.id);
-
-    const user = users[id];
-    
-    return response.json(user);
-});
-
-app.post('/users', (request, response) => {
-    const data = request.body;
-
-    console.log(data);
-
-    const user = {
-        name: data.name,
-        email: data.email
-    };
-
-    return response.json(user);
-});
-
- */
+app.listen(3333)
